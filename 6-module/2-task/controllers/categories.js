3dff52995db905328fbe811341e5119b76bd8208
Dmitry Peshkin <dmitrey04@gmail.com>
2019-07-14 16:58:02 +0500

    add solution for 6-module 2-task

--- 6-module/2-task/controllers/categories.js
+++ 6-module/2-task/controllers/categories.js
@@ -1,3 +1,11 @@
+const Category = require('../models/Category');
+
 module.exports.categoryList = async function categoryList(ctx, next) {
-  ctx.body = { categories: [] };
+  const categories = await Category.find();
+  const categoriesToObject = categories.map((cat) => {
+    const category = cat.toObject();
+    category.subcategories = cat.subcategories.map((subCat) => subCat.toObject());
+    return category;
+  });
+  ctx.body = {categories: categoriesToObject};
 };
--- 6-module/2-task/controllers/products.js
+++ 6-module/2-task/controllers/products.js
@@ -1,12 +1,34 @@
+const mongoose = require('mongoose');
+const Product = require('../models/Product');
+
 module.exports.productsBySubcategory = async function productsBySubcategory(ctx, next) {
-  ctx.body = { products: [] };
+  const {subcategory} = ctx.query;
+  if (mongoose.Types.ObjectId.isValid(subcategory)) {
+    const products = await Product.find({subcategory: subcategory});
+    ctx.body = {products: products.map((el) => el.toObject())};
+  } else {
+    ctx.body = {products: []};
+    ctx.status = 400;
+  }
 };
 
 module.exports.productList = async function productList(ctx, next) {
-  ctx.body = { products: [] };
+  const products = await Product.find();
+  ctx.body = {products: products.map((el) => el.toObject())};
 };
 
 module.exports.productById = async function productById(ctx, next) {
-  ctx.body = { product: {} };
+  const {id} = ctx.params;
+  if (mongoose.Types.ObjectId.isValid(id)) {
+    const product = await Product.findById(id);
+    if (product) {
+      ctx.body = {product: product.toObject()};
+    } else {
+      ctx.body = 'NotFound';
+      ctx.status = 404;
+    }
+  } else {
+    ctx.body = 'WrongId';
+    ctx.status = 400;
+  }
 };
-
